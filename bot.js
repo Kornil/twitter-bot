0a98f334054f24ae7bff87da8c125016674c406e
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2017-03-02 18:17:21 +0100

    initial commit

---
+++ bot.js
@@ -0,0 +1,49 @@
+var twit = require('twit');
+
+var config = require('./config.js');
+
+var Twitter = new twit(config);
+
+// FAVORITE BOT====================
+
+// find a random tweet and 'favorite' it
+var favoriteTweet = function(){
+  var params = {
+      q: '#100DaysOfCode',  // REQUIRED
+      result_type: 'recent',
+      lang: 'en'
+  }
+  // for more parametes, see: https://dev.twitter.com/rest/reference
+
+  // find the tweet
+  Twitter.get('search/tweets', params, function(err,data){
+
+    // find tweets
+    var tweet = data.statuses;
+    var randomTweet = ranDom(tweet);   // pick a random tweet
+
+    // if random tweet exists
+    if(typeof randomTweet != 'undefined'){
+      // Tell TWITTER to 'favorite'
+      Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
+        // if there was an error while 'favorite'
+        if(err){
+          console.log('CANNOT BE FAVORITE... Error');
+        }
+        else{
+          console.log('FAVORITED... Success!!!');
+        }
+      });
+    }
+  });
+}
+// grab & 'favorite' as soon as program is running...
+favoriteTweet();
+// 'favorite' a tweet in every 5 minutes
+setInterval(favoriteTweet, 1000 * (60 * 5));
+
+// function to generate a random tweet tweet
+function ranDom (arr) {
+  var index = Math.floor(Math.random()*arr.length);
+  return arr[index];
+};
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: PROCESS.ENV.CONSUMER_KEY,  
+  consumer_secret: PROCESS.ENV.CONSUMER_SECRET,
+  access_token: PROCESS.ENV.ACCESS_TOKEN,  
+  access_token_secret: PROCESS.ENV.ACCESS_TOKEN_SECRET
+}
