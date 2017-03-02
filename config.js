f1f11f69f5e3e33bbefc53288ed9a67f1d66437e
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2017-03-02 18:22:08 +0100

    uppercase typo

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: PROCESS.ENV.CONSUMER_KEY,  
-  consumer_secret: PROCESS.ENV.CONSUMER_SECRET,
-  access_token: PROCESS.ENV.ACCESS_TOKEN,  
-  access_token_secret: PROCESS.ENV.ACCESS_TOKEN_SECRET
+  consumer_key: process.env.CONSUMER_KEY,  
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,  
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 }
